ddd4c04993cdace6832c611ec46b7d455f969cce
차일권 <luck444u@naver.com>
2025-11-25 13:28:52 +0900

    2025 11 18

---
+++ C-Programming-XI/work17.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+#define pi 3.141592
+
+int d();
+
+int main()
+{
+    d();
+    return 0;
+}
+
+int d()
+{
+    int angle;
+    double rad, s, c;
+    int posS, posC;
+    int i;
+
+    for (angle = 0; angle <= 360; angle += 15) {
+
+        rad = angle * pi / 180.0;
+        s = sin(rad);
+        c = cos(rad);
+
+        posS = (int)((s + 1) * 25);
+        posC = (int)((c + 1) * 25);
+
+        for (i = 0; i < 51; i++) {
+            if (i == posS && i == posC)
+                printf("B");
+            else if (i == posS)
+                printf("S");
+            else if (i == posC)
+                printf("C");
+            else
+                printf(" ");   // ← 점 대신 공백
+        }
+
+        printf("  %3d°\n", angle);
+    }
+
+    return 0;
+}
